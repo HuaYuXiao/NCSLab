1b21d37c1207c554f166df750efc012813bf10e5
Eason Hua <hyx020222@gmail.com>
2024-03-30 16:56:48 +0800

    Update

--- src/Server/Server.py
+++ src/Server/Server.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_socketio import SocketIO, emit
 import subprocess
 
 
-app = Flask(__name__, template_folder='/home/hyx020222/NCSLab/src/Server/')
+app = Flask(__name__, template_folder='/home/hyx020222/NCSLab/src/Server/template')
 
 
 @app.route('/')
@@ -11,16 +12,22 @@ def index():
 
 @app.route('/start')
 def start():
-    subprocess.Popen(['python3', 'src/main.py'])
+    subprocess.Popen(['python3', 'src/hello.py'])
     return redirect(url_for('start_page'))
 
 @app.route('/start_page')
 def start_page():
     return render_template('start_page.html')
 
+@app.route('/camera')
+def start_camera():
+    # ??????????????????
+    # ????? Flask-SocketIO ?????????????
+    return redirect(url_for('start_page'))
+
 @app.route('/stop')
 def stop():
-    subprocess.Popen(['pkill', '-f', 'src/main.py'])
+    subprocess.Popen(['pkill', '-f', 'src/hello.py'])
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
