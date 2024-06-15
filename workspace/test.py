4e873319566598a9166baf9f8788b106dd92cf02
EasonHua <1628280289@qq.com>
2024-06-15 18:12:24 +0800

    [update | pass | v2.2.0] load model on website

--- workspace/test.py
+++ workspace/test.py
@@ -17,14 +17,37 @@ import threading
 from PIL import Image
 import io
 
+
 app = Flask(__name__)
+socketio = SocketIO(app)
+# ??????????????
+stream = BytesIO()
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/main', methods=['GET', 'POST'])
+def main():
+    if request.method == 'POST':
+        # Get the values from the input boxes
+        value1 = request.form.get('input1')
+        value2 = request.form.get('input2')
+        value3 = request.form.get('input3')
+
+        print(value1, value2, value3)
+
+        # Redirect back to the main page or to a different page if needed
+        return redirect(url_for('main'))
+
+    return render_template('main.html')
+
+@app.route('/stop')
+def stop():
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
         # Use the SocketIO run method to handle both the web server and SocketIO events
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8090, debug=True)
